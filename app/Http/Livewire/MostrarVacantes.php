bab38d18853e0cff523764503adaecd8e45dfa31
Fernando Acosta <lacosta@gobdigital.com>
2022-07-18 17:59:54 -0500

    Eliminacion de vacantes

--- app/Http/Livewire/MostrarVacantes.php
+++ app/Http/Livewire/MostrarVacantes.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire;
 
 use App\Models\Vacante;
+use App\Services\ImageService;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class MostrarVacantes extends Component
 {
+    protected $listeners = ['eliminarVacante'];
+
+    public function eliminarVacante(Vacante $vacante)
+    {
+        $vacante->delete();
+        ImageService::deleteImage($vacante->imagen);
+    }
+
     public function render()
     {
         $vacantes = Vacante::where('user_id', Auth::id())->paginate(10);
--- resources/views/vacantes/livewire/mostrar-vacantes.blade.php
+++ resources/views/vacantes/livewire/mostrar-vacantes.blade.php
@@ -1,38 +1,66 @@
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="space-y-3">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
 
-    @forelse ($vacantes as $vacante)
-        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
-            <div class="space-y-3">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante->titulo }}
-                </a>
+                    <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+                    <p class="text-sm text-gray-500">Último día: {{ $vacante->last_day->format('d/m/Y') }}</p>
+                </div>
 
-                <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
-                <p class="text-sm text-gray-500">Último día: {{ $vacante->last_day->format('d/m/Y') }}</p>
+                <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+                    <a href="#"
+                       class="text-center bg-slate-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
+                        Candidatos
+                    </a>
+
+                    <a href="{{ route('vacantes.edit', $vacante) }}"
+                       class="text-center bg-blue-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
+                        Editar
+                    </a>
+
+                    <button wire:click="$emit('deleteVacante', {{ $vacante->id }})"
+                            class="text-center bg-red-600 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
+                        Eliminar
+                    </button>
+                </div>
             </div>
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">No hay Vacantes</p>
+        @endforelse
+    </div>
 
-            <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
-                <a href="#"
-                   class="text-center bg-slate-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
-                    Candidatos
-                </a>
-
-                <a href="{{route('vacantes.edit', $vacante)}}"
-                   class="text-center bg-blue-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
-                    Editar
-                </a>
-
-                <a href="#"
-                   class="text-center bg-red-600 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase">
-                    Eliminar
-                </a>
-            </div>
-        </div>
-    @empty
-        <p class="p-3 text-center text-sm text-gray-600">No hay Vacantes</p>
-    @endforelse
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
 </div>
 
-<div class="mt-10">
-    {{ $vacantes->links() }}
-</div>
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        Livewire.on('deleteVacante', (vacanteId) => {
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: "¡No podrás revertir esto!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminarlo!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emit('eliminarVacante', vacanteId);
+                    Swal.fire(
+                        'Eliminado!',
+                        'La vacante ha sido eliminada.',
+                        'success'
+                    )
+                }
+            })
+        })
+    </script>
+@endpush
